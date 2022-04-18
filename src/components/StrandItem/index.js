395ddedfa12566d9f06e9a869c81e9e0c9fd0299
Waigwa <mail@waigs.me>
2022-04-18 11:52:12 +0100

    Adding link path to the strands

--- src/components/StrandItem/index.js
+++ src/components/StrandItem/index.js
@@ -1,13 +1,13 @@
 import './stranditem.scss';
 
-export const StrandItem = ({ color, title, description }) => {
+export const StrandItem = ({ color, title, description, path }) => {
   return (
     <div className={`campus-stranditem-wrapper ${color}`}>
       <div className="campus-container">
-        <div className="campus-strand-item">
+        <a href={path} className="campus-strand-item">
           <h5 className="campus-subtitle font-semi small no-margin">{title}</h5>
           <span className="campus-subtitle small no-margin">{description}</span>
-        </div>
+        </a>
       </div>
     </div>
   );
